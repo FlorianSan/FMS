44a37ce7fae572996c7ea033ac971bf7edf427da
luscawalker <45207184+luscawalker@users.noreply.github.com>
2021-02-03 17:29:12 +0100

    ajout install GUID_COMM

--- install.sh
+++ install.sh
@@ -37,6 +37,20 @@ cd NavDB/
 chmod +x navdb.sh
 ./navdb.sh
 
+######################################
+# installation relative to GUID_COMM #
+######################################
+cd ..
+sudo apt update
+sudo apt install build-essential
+sudo apt-get install xorg openbox
+sudo apt-get install libxt-dev
+sudo apt-get install libpcre3-dev
+sudo apt-get install tcl8.6-dev
+sudo apt-get install libglib2.0-dev
+cd GUID_COMM/ivy-c_3.15.1/src/ 
+make
+sudo make install
 
 echo "Installation terminé"
 
