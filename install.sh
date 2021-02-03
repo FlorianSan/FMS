2036a61a5691b370a71691121abd2878b628c686
luscawalker <deloye98@gmail.com>
2021-02-03 18:31:24 +0100

    Rajout version package python

--- install.sh
+++ install.sh
@@ -22,11 +22,11 @@ sudo apt install -y openjdk-11-jre python3-pip python3-tk python3-pil python3-pi
 sudo apt-get install -y --reinstall libpq-dev
 sudo apt-get install -y --reinstall libxcb-xinerama0
 
-python3 -m pip install ivy-python
-python3 -m pip install PyQt5
-python3 -m pip install matplotlib
-python3 -m pip install psycopg2
-python3 -m pip install pyproj
+python3 -m pip install ivy-python==3.3
+python3 -m pip install PyQt5=5.15.2
+python3 -m pip install matplotlib==3.3.4
+python3 -m pip install psycopg2==2.8.6
+python3 -m pip install pyproj==3.0.0.post1
 python3 -m pip install Pillow
 
 #################
