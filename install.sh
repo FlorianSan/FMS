ae48de23b62883cdea4e46dc9982fa6099051556
FlorianSan <fl.sansou@gmail.com>
2021-02-03 16:26:45 +0100

    m

--- install.sh
+++ install.sh
@@ -19,13 +19,15 @@
 # Dependicies
 #################
 sudo apt install -y openjdk-11-jre python3-pip python3-tk
-sudo apt-get install --reinstall libpq-dev
+sudo apt-get install -y --reinstall libpq-dev
+sudo apt-get install -y --reinstall libxcb-xinerama0
 
 python3 -m pip install ivy-python
 python3 -m pip install PyQt5
 python3 -m pip install matplotlib
 python3 -m pip install psycopg2
-python3 -m pip pip install pyproj
+python3 -m pip install pyproj
+python3 -m pip install Pillow
 
 #################
 # Database
