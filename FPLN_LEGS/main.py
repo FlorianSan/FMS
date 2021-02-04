ce73bf5df097f7d4729acf1f59b43e60813c65c3
RomainT41 <78029929+RomainT41@users.noreply.github.com>
2021-02-04 14:53:30 +0100

    Update main.py

--- FPLN_LEGS/main.py
+++ FPLN_LEGS/main.py
@@ -2,22 +2,25 @@ import Route
 import FlightPlan
 import threading
 import IhmManager
+import DatabaseManager
 import CommunicationManager
 
-def ihm(FPLN_Route, FPLN_Leg, FPLN_temporary, communicationManager):
-    ihmManager = IhmManager.IhmManager(FPLN_Route, FPLN_Leg, FPLN_temporary, communicationManager)
+def ihm(FPLN_Route, FPLN_Leg, FPLN_temporary, communicationManager, databaseManager):
+    ihmManager = IhmManager.IhmManager(FPLN_Route, FPLN_Leg, FPLN_temporary, communicationManager, databaseManager)
     ihmManager.run()
     pass
 
 if __name__=="__main__":
 
+    databaseManager = DatabaseManager.Database_Manager()
+    
     FPLN_Route = Route.Route()
-    FPLN_Leg = FlightPlan.FlightPlan()
-    FPLN_temporary = FlightPlan.FlightPlan()
+    FPLN_Leg = FlightPlan.FlightPlan(databaseManager)
+    FPLN_temporary = FlightPlan.FlightPlan(databaseManager)
 
     communicationManager = CommunicationManager.CommunicationManager(FPLN_Route, FPLN_Leg, FPLN_temporary)
 
-    ihmThread = threading.Thread(target=ihm, args=(FPLN_Route, FPLN_Leg, FPLN_temporary, communicationManager,))
+    ihmThread = threading.Thread(target=ihm, args=(FPLN_Route, FPLN_Leg, FPLN_temporary, communicationManager, databaseManager))
     ihmThread.start()
 
     communicationManager.run()
